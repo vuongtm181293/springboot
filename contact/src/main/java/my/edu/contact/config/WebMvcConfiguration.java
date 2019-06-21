cc7e6bfdf7beda3d97d514566b14c25752564083
Truong Minh Vuong <vuongtm181293@gmail.com>
2019-06-20 21:53:01 -0500

    im need sleeping

--- contact/src/main/java/my/edu/contact/config/WebMvcConfiguration.java
+++ contact/src/main/java/my/edu/contact/config/WebMvcConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -15,6 +16,14 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 		messageSource.addBasenames("classpath:messages");
 		messageSource.setDefaultEncoding("UTF-8");
 		return messageSource;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		if (!registry.hasMappingForPattern("/static/**")) {
+			registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+		}
+
 	}
 
 }
